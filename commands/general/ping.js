ce91b8a1eb4d2adde1af3cff90fcd1ad5e1342bc
SigumaRoot <sigumaroot@outlook.jp>
2022-08-10 05:01:32 +0000

    a

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -3,19 +3,21 @@ const Discord = require("discord.js");
 const { ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
 
 module.exports = {
-    guildOnly: false, // サーバー専用コマンドかどうか
-    data: new SlashCommandBuilder() // スラッシュコマンド登録のため
-        .setName("ping")
-        .setDescription("Ping値を測定"),
+  guildOnly: false, // サーバー専用コマンドかどうか
+  data: new SlashCommandBuilder() // スラッシュコマンド登録のため
+    .setName("ping")
+    .setDescription("Ping値を測定"),
 
-    async execute(i, client) {
-        const embed = new Discord.Embed()
-                .setTitle("Ping")
-                .setDescription("Pong!")
-                .addField("WebSocket", `**${client.ws.ping} ms**`, true)
-                .addField("コマンド受信", `**${new Date() - i.createdAt} ms**`, true)
-                .setColor(client.config.color)
-                .setTimestamp();
-        i.reply({embeds: [embed]});
-    },
+  async execute(i, client) {
+    const cmdPing = new Date() - i.createdAt;
+    const embed = new Discord.EmbedBuilder()
+      .setTitle("Ping")
+      .setDescription("Pong!")
+      .addFields([
+        { name: 'WebSocket', value: ` ** ${client.ws.ping} ms ** ` }, 
+        { name: 'コマンド受信', value: `** ${ cmdPing } ms ** ` }])
+      .setColor(client.config.color)
+      .setTimestamp();
+    i.reply({ embeds: [embed] });
+  },
 }
