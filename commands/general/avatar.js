5c4f1d36a90c7bf54bb798010c6c00bb171a127a
SigumaRoot <sigumaroot@outlook.jp>
2022-08-10 09:20:14 +0000

    a

---
+++ commands/general/avatar.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require("discord.js");
+const Discord = require("discord.js");
+const { ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+  guildOnly: false, // サーバー専用コマンドかどうか
+  data: new SlashCommandBuilder() // スラッシュコマンド登録のため
+    .setName("avatar")
+    .setDescription('Get the avatar URL of the selected user, or your own avatar.')
+    .addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
+  async execute(i, client) {
+    const user = i.options.getUser('target');
+    if (user) {
+      const embed = new Discord.EmbedBuilder()
+      .setTitle(`${user.username}'s avatar`)
+      .setDescription(`${user.displayAvatarURL({ dynamic: true })}`)
+      .setColor(client.config.color)
+      .setImage(user.displayAvatarURL({ dynamic: true }))
+      .setTimestamp();
+      return i.reply({ embeds: [embed] })}
+    const embed = new Discord.EmbedBuilder()
+      .setTitle(`Your avatar.`)
+      .setDescription(`${i.user.displayAvatarURL({ dynamic: true })}`)
+      .setImage(i.user.displayAvatarURL({ dynamic: true }))
+      .setColor(client.config.color)
+      .setTimestamp();
+      return i.reply({ embeds: [embed] })
+  },
+}
