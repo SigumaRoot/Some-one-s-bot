44a4c1f8a62633cece57699071a9428444fe3e7b
SigumaRoot <sigumaroot@outlook.jp>
2022-08-17 15:08:02 +0900

    dnd

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,9 @@ const functions = require("../functions.js");
 module.exports = {
     name: "ready", // イベント名
     async execute(client) {
-        client.user.setPresence({ activities: [{ name: `Type /help | Servers: ${client.guilds.cache.size}` }], status: "did" });
+        client.user.setPresence({ activities: [{ name: `Type /help | Servers: ${client.guilds.cache.size}` }]});
         client.channels.cache.get(config.logch.ready).send("Discordログインしました！");
         console.log(`[${functions.timeToJST(Date.now(), true)}] Logged in as ${client.user.tag}!`);
+        client.user.setStatus('dnd');
     }
 }
