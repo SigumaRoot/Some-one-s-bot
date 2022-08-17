5885a00e1081dc1d787e11fb220a5996dab9112c
SigumaRoot <sigumaroot@outlook.jp>
2022-08-17 18:21:21 +0900

    reboot & add.jsの自動化

---
+++ commands/admin/reboot.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
+const Discord = require("discord.js");
+const { ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+  guildOnly: false, // サーバー専用コマンドかどうか
+  adminGuildOnly: true,
+  data: new SlashCommandBuilder() 
+    .setName("reboot")
+    .setDescription("reboot"),
+
+  async execute(i, client) {
+    client.destroy();
+    client.login(process.env.token);
+  },
+}
--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,8 @@ const functions = require("../functions.js");
 module.exports = {
     name: "ready", // イベント名
     async execute(client) {
+        const add = require(`../add.js`);
+        add.addCmd(client.config);
         client.user.setPresence({ activities: [{ name: `Type /help | Servers: ${client.guilds.cache.size}` }], status: "did" });
         client.channels.cache.get(config.logch.ready).send("Discordログインしました！");
         console.log(`[${functions.timeToJST(Date.now(), true)}] Logged in as ${client.user.tag}!`);
