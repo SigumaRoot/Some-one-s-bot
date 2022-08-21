9980cdb2e4d303870c0a0eab3a18c16c481952d1
SigumaRoot <sigumaroot@outlook.jp>
2022-08-21 02:46:27 +0000

    Siguma-chan -> Replit

--- index.js
+++ index.js
@@ -17,24 +17,8 @@ const functions = require("./functions.js");
 client.func = functions;
 client.config = config;
 
-// コマンドハンドリング
-client.commands = new Collection();
-const commandFolders = fs.readdirSync("./commands");
-for (const folder of commandFolders) {
-  console.log(`\u001b[32m===${folder} commands===\u001b[0m`);
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
-    try {
-      client.commands.set(command.data.name, command);
-      console.log(`${command.data.name} がロードされました。`);
-    } catch (error) {
-      console.log(`\u001b[31m${command.data.name} はエラーによりロードされませんでした。\nエラー内容\n ${error}\u001b[0m`);
-    }
-  }
-  console.log(`\u001b[32m===${folder} loaded===\u001b[0m`);
-}
-
+const cmdH=require(`./system/command.js`);
+cmdH.handling(client,fs,Collection,config);
 // イベントハンドリング
 const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
 for (const file of eventFiles) {
@@ -93,9 +77,6 @@ client.on("interactionCreate", async i => {
 });
 // コmsg
 client.on("messageCreate", async msg => {
-  console.log(msg.content.indexOf(`s!`) !== 0);
-  console.log('content:' + msg.content);
-  console.log('-s!>' + msg.content.replace('s!', ''));
   if (msg.content.indexOf(`s!`) !== 0) return;
   const command = client.commands.get(msg.content.replace('s!', ''));
   if (!command) return;
