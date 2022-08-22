417caaf27a76c062c5c34dfa437239a23f07d480
SigumaRoot <sigumaroot@outlook.jp>
2022-08-22 08:45:41 +0000

    logから読む。

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,15 @@
 const config = require("../config.js");
 const functions = require("../functions.js");
+const fs = require('fs');
 
 module.exports = {
   name: "ready", // イベント名
   async execute(client) {
+    let log = JSON.parse(fs.readFileSync(`../log/maintenance.log`, 'utf8'));
     const add = require(`../system/add.js`);
     add.addCmd(client.config);
-    client.user.setActivity(`Type /help | Servers: ${client.guilds.cache.size}`);
-    client.user.setStatus('online');
+    client.user.setActivity(log.playing);
+    client.user.setStatus(log.status);
     /*client.user.setActivity('activity', { type: 'WATCHING' });
 client.user.setActivity('activity', { type: 'LISTENING' });
 client.user.setActivity('activity', { type: 'COMPETING' });*/
