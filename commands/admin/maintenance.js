0a5aff33f3ed9b6da369852b16f320e863248ed6
SigumaRoot <sigumaroot@outlook.jp>
2022-08-22 13:53:29 +0900

    メンテナンスにメセージ追加できるようにする。

--- commands/admin/maintenance.js
+++ commands/admin/maintenance.js
@@ -8,14 +8,22 @@ module.exports = {
   data: new SlashCommandBuilder() // スラッシュコマンド登録のため
     .setName("maintenance")
     .setDescription("メンテモード")
-    .addStringOption(option => option.setName('status').setDescription('すたーてす')),
+    .addStringOption(option => option.setName('status').setDescription('すたーてす'))
+    .addStringOption(option => option.setName('Playing').setDescription('プレイ中に表示するやつ')),
 
   async execute(i, client,command) {
-    const status=i.options.getString('status');    
+    const status=i.options.getString('status');
+    const playing=i.options.getString('Playing');
+    client.user.setActivity(playing);
     client.user.setStatus(status);
       const embed = new Discord.EmbedBuilder()
       .setTitle("ok")
       .setColor(client.config.color.s)
+      .addFields([{
+        name:"Status",value:client.user.status
+      },{
+        name:"Playing",value:playing
+      }])
       .setTimestamp();
     
     return i.reply({ embeds: [embed] });
