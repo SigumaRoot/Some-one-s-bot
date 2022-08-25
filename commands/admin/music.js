e8780cdef07f45522f1fc39ce35530b58b607f66
SigumaRoot <sigumaroot@outlook.jp>
2022-08-26 03:45:10 +0900

    bug

--- commands/admin/music.js
+++ commands/admin/music.js
@@ -2,89 +2,87 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { QueryType } = require("discord-player");
 
 module.exports = {
-  guildOnly: false, // サーバー専用コマンドかどうか
-  adminGuildOnly: true,
+    guildOnly: false, // サーバー専用コマンドかどうか
+    adminGuildOnly: true,
 
-  data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("音楽を再生します")
-    .addStringOption((option) =>
-      option.setName("url").setDescription("YouTube URL").setRequired(true)
-    ),
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("音楽を再生します")
+        .addStringOption((option) =>
+            option.setName("url").setDescription("YouTube URL").setRequired(true)
+        ),
 
-  async execute(i, client) {
-    const bot = await i.guild.members.resolve(client.user.id);
-    if (!i.member.voice.channelId) {
-      await i.reply({
-        content: "ボイスチャンネルに参加してください",
-        ephemeral: true,
-      });
-      return 'No data';
-    }
+    async excute(client, interaction) {
+        if (!interaction.member.voice.channelId) {
+            await interaction.reply({
+                content: "ボイスチャンネルに参加してください",
+                ephemeral: true,
+            });
+            return 'No data';
+        }
 
-    if (
-      bot.voice.channelId &&
-      i.member.voice.channelId !==
-      bot.voice.channelId
-    ) {
-      await i.reply({
-        content: "botと同じボイスチャンネルに参加してください",
-        ephemeral: true,
-      });
-      return 'No data';
-    }
+        if (
+            interaction.guild.me.voice.channelId &&
+            interaction.member.voice.channelId !==
+            interaction.guild.me.voice.channelId
+        ) {
+            await interaction.reply({
+                content: "botと同じボイスチャンネルに参加してください",
+                ephemeral: true,
+            });
+            return 'No data';
+        }
 
-    // キューを生成
-    const queue = client.player.createQueue(i.guild, {
-      metadata: {
-        channel: i.channel,
-      },
-    });
+        // キューを生成
+        const queue = client.player.createQueue(interaction.guild, {
+            metadata: {
+                channel: interaction.channel,
+            },
+        });
 
-    try {
-      // VCに入ってない場合、VCに参加する
-      if (!queue.connection) {
-        await queue.connect(i.member.voice.channel);
-      }
-    } catch {
-      queue.destroy();
-      await i.reply({
-        content: "ボイスチャンネルに参加できませんでした",
-        ephemeral: true,
-      });
-      return 'No data';
-    }
+        try {
+            // VCに入ってない場合、VCに参加する
+            if (!queue.connection) {
+                await queue.connect(interaction.member.voice.channel);
+            }
+        } catch {
+            queue.destroy();
+            await interaction.reply({
+                content: "ボイスチャンネルに参加できませんでした",
+                ephemeral: true,
+            });
+            return 'No data';
+        }
 
-    await i.deferReply();
-    console.log('defer');
+        await interaction.deferReply();
 
-    const url = i.options.getString("url");
-    // 入力されたURLからトラックを取得
-    const track = await client.player
-      .search(url, {
-        requestedBy: i.user,
-        searchEngine: QueryType.YOUTUBE_VIDEO,
-      })
-      .then((x) => x.tracks[0]);
-    if (!track) {
-      console.log('not found');
-      await i.editReply(
-        '動画が見つかりませんでした');
-      return 'No data';
-    }
+        const url = interaction.options.getString("url");
+        // 入力されたURLからトラックを取得
+        const track = await client.player
+            .search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO,
+            })
+            .then((x) => x.tracks[0]);
 
-    // キューにトラックを追加
-    await queue.addTrack(track);
-    console.log('added');
+        if (!track) {
+            await interaction.followUp({
+                content: "動画が見つかりませんでした",
+            });
+            return 'No data';
+        }
 
-    // 音楽が再生中ではない場合、再生
-    if (!queue.playing) {
-      queue.play();
-      console.log('play track');
-    }
+        // キューにトラックを追加
+        await queue.addTrack(track);
 
-    await i.editReply(
-      `音楽をキューに追加しました **${track.title}**`);
-    return 'No data';
-  },
+        // 音楽が再生中ではない場合、再生
+        if (!queue.playing) {
+            queue.play();
+        }
+
+        await interaction.followUp({
+            content: `音楽をキューに追加しました **${track.title}**`,
+        });
+        return 'No data';
+    },
 };
