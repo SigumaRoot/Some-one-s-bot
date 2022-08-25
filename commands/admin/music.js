349f840880083c318f8559f3d6f844bb2a64d99e
SigumaRoot <sigumaroot@outlook.jp>
2022-08-25 19:35:42 +0000

    revert

--- commands/admin/music.js
+++ commands/admin/music.js
@@ -12,41 +12,55 @@ module.exports = {
             option.setName("query").setDescription("YouTube URL").setRequired(true)
         ),
 
-    async execute(ctx,client) {
-        await ctx.defer();
-
-        const guild = client.guilds.cache.get(ctx.guildID);
-        const channel = guild.channels.cache.get(ctx.channelID);
-        const query = ctx.options.query;
-        const searchResult = await client.player
-            .search(query, {
-                requestedBy: ctx.user,
-                searchEngine: QueryType.AUTO
-            })
-            .catch(() => {
-                console.log('he');
-            });
-        if (!searchResult || !searchResult.tracks.length) return void ctx.sendFollowUp({ content: 'No results were found!' });
-
-        const queue = await client.player.createQueue(guild, {
-            ytdlOptions: {
-                filter: 'audioonly',
-                highWaterMark: 1 << 30,
-                dlChunkSize: 0,
-            },
-            metadata: channel
-        });
-
-        const member = guild.members.cache.get(ctx.user.id) ?? await guild.members.fetch(ctx.user.id);
-        try {
-            if (!queue.connection) await queue.connect(member.voice.channel);
-        } catch {
-            void client.player.deleteQueue(ctx.guildID);
-            return void ctx.sendFollowUp({ content: 'Could not join your voice channel!' });
+    async execute(interaction, client) {
+        if (!interaction.member.voice.channelId) {
+            await interaction.reply({ content: "vcに参加してください！！", ephemeral: true });
+            return 'No data'
+        }
+        if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) {
+            await interaction.reply({ content: "ボットと同じvcに参加してください！！", ephemeral: true });
+            return 'No data';
         }
 
-        await ctx.sendFollowUp({ content: `⏱ | Loading your ${searchResult.playlist ? 'playlist' : 'track'}...` });
-        searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
-        if (!queue.playing) await queue.play();
-    }
+        const query = interaction.options.getString("query");
+        const queue = client.player.createQueue(interaction.guild, {
+            metadata: {
+                channel: interaction.channel
+            }
+        });
+
+        // verify vc connection
+        try {
+            if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+        } catch {
+            queue.destroy();
+            await interaction.reply({ content: "すみません。vcに参加できませんでした...", ephemeral: true });
+            return 'No data';
+        }
+
+        //await interaction.deferReply();
+
+        // 入力されたURLからトラックを取得
+        const track = await client.player.search(query, {
+            requestedBy: interaction.user
+        }).then(x => x.tracks[0]);
+
+        if (!track) {
+            await interaction.reply({ content: `❌ | **${query}**は見つかりませんでした...` })
+            return 'No data';
+        }
+
+        // キューにトラックを追加
+        await queue.addTrack(track);
+
+        // 音楽が再生中ではない場合、再生
+        if (!queue.playing) {
+            queue.play();
+        }
+
+        await interaction.reply({
+            content: `音楽をキューに追加しました **${track.title}**`,
+        });
+        return 'No data';
+    },
 };
