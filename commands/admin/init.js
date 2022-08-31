ce09b3caeb4f1e6d0e9ee99853069baa37dd9484
SigumaRoot <sigumaroot@outlook.jp>
2022-08-31 18:03:29 +0900

    bug

--- commands/admin/init.js
+++ commands/admin/init.js
@@ -2,24 +2,24 @@ const { SlashCommandBuilder } = require("discord.js");
 const Discord = require("discord.js");
 
 module.exports = {
-    guildOnly: false, // サーバー専用コマンドかどうか
-    adminGuildOnly: true,
+    guildsOnly: false, // サーバー専用コマンドかどうか
+    adminguildsOnly: true,
     data: new SlashCommandBuilder()
         .setName("init")
         .setDescription("サーバーでボットを使えるようにします。"),
 
     async execute(i, client) {
-        let category = i.guild.channels.cache.find(c => c.type == "GUILD_CATEGORY" && c.name == 'Bot Config');
-        let channels = i.guild.channels.cache.find(c => c.type == "GUILD_TEXT" && c.name == client.user.username);
+        let category = i.guilds.channels.cache.find(c => c.type == "guilds_CATEGORY" && c.name == 'Bot Config');
+        let channels = i.guilds.channels.cache.find(c => c.type == "guilds_TEXT" && c.name == client.user.username);
         if (category) {
             let rep = new Discord.EmbedBuilder().setTitle('このサーバーにはもう設定用のチャンネルがあります！！');
             i.return({ embeds: [category] });
             return 'No data';
         } else {
-            i.guild.channels.create('Bot Config', { type: "GUILD_CATEGORY" });
-            i.guild.channels.create(client.user.username);
-            /*let configCh = i.guild.channels.cache.find(client.user.username);
-            let role = i.guilds.roles.fetch();
+            i.guilds.channels.create('Bot Config', { type: "guilds_CATEGORY" });
+            i.guilds.channels.create(client.user.username);
+            /*let configCh = i.guilds.channels.cache.find(client.user.username);
+            let role = i.guildss.roles.fetch();
             configCh.permissionOverwrites.set([
                 {
                     id: role, // またはそれらのオブジェクト
@@ -27,7 +27,7 @@ module.exports = {
                     //type: "IDを指定する場合は必要" // role or member
                 }
             ], 'Config channel.');*/
-            i.reply('sucsess.')
+            i.reply('sucsess.');
         }
     },
 }
