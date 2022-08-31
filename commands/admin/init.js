c486e85a058891dfdb4980f8de2704c6efd24a29
SigumaRoot <sigumaroot@outlook.jp>
2022-08-31 20:03:31 +0000

    いろいろやった。

--- commands/admin/init.js
+++ commands/admin/init.js
@@ -2,32 +2,32 @@ const { SlashCommandBuilder } = require("discord.js");
 const Discord = require("discord.js");
 
 module.exports = {
-    guildsOnly: false, // サーバー専用コマンドかどうか
-    adminguildsOnly: true,
+    guildOnly: false, // サーバー専用コマンドかどうか
+    adminguildOnly: true,
     data: new SlashCommandBuilder()
         .setName("init")
         .setDescription("サーバーでボットを使えるようにします。"),
 
     async execute(i, client) {
-        let category = i.guilds.channels.cache.find(c => c.type == "guilds_CATEGORY" && c.name == 'Bot Config');
-        let channels = i.guilds.channels.cache.find(c => c.type == "guilds_TEXT" && c.name == client.user.username);
+        let category = i.guild.channels.cache.find(c => c.type == "GUILD_CATEGORY" && c.name == 'Bot Config');
+        let channels = i.guild.channels.cache.find(c => c.type == "GUILD_TEXT" && c.name == client.user.username);
         if (category) {
+          console.log('1');
             let rep = new Discord.EmbedBuilder().setTitle('このサーバーにはもう設定用のチャンネルがあります！！');
             i.return({ embeds: [category] });
             return 'No data';
         } else {
-            i.guilds.channels.create('Bot Config', { type: "guilds_CATEGORY" });
-            i.guilds.channels.create(client.user.username);
-            /*let configCh = i.guilds.channels.cache.find(client.user.username);
-            let role = i.guildss.roles.fetch();
-            configCh.permissionOverwrites.set([
+            i.guild.channels.create( { name:'Bot Config',type: 'GUILD_CATEGORY' });
+            //i.guild.channels.create(client.user.username);
+      i.guild.channels.create({ name: client.user.username, reason: 'Bot Config(Do　not edit！)' })
+            //let role = i.guilds.roles.fetch();
+            /*configCh.permissionOverwrites.set([
                 {
                     id: role, // またはそれらのオブジェクト
                     deny: 535529191505, // 許可しない権限
-                    //type: "IDを指定する場合は必要" // role or member
                 }
-            ], 'Config channel.');*/
-            i.reply('sucsess.');
+            ], 'Config channel.');
+            i.reply('sucsess.');*/
         }
     },
 }
