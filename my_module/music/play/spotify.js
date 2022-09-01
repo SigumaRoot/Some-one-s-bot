04e3f2ad6327f9d8bac9208fff0e38de40739d6a
SigumaRoot <sigumaroot@outlook.jp>
2022-09-01 17:08:45 +0900

    npm

--- my_module/music/play/spotify.js
+++ my_module/music/play/spotify.js
@@ -1,11 +1,25 @@
 const { entersState, AudioPlayerStatus, createAudioPlayer, createAudioResource, joinVoiceChannel, StreamType } = require('@discordjs/voice');
-const play = require('play-dl');
-const { video_basic_info, stream } = require('play-dl');
-const  SpottyDL  =  require ( 'spottydl' ) 
+const fetch = require('isomorphic-unfetch')
+const { getData, getPreview, getTracks, getDetails } = require('spotify-url-info')(fetch);
+const ytdl = require(`./youtube`);
+const yts = require('yt-search')
+
+
 
 module.exports = {
-    async execute(i, query) { 
-
+    async execute(i, query) {
+        const title = getPreview(query).title;
+        const type = getPreview(query).type;
+        switch (type) {
+            case 'track':
+                const r = await yts(title);
+                const videos = r.videos.slice(0, 3)
+                videos.forEach(function (v) {
+                    const views = String(v.views).padStart(10, ' ')
+                    console.log(`${views} | ${v.title} (${v.timestamp}) | ${v.author.name}`)
+                });
+                ytdl.execute(i,);
+        }
         return 'No data';
     }
 }
