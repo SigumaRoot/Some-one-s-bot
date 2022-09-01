53b04165644815a64851d64a290d6f085f696910
SigumaRoot <sigumaroot@outlook.jp>
2022-09-01 08:06:14 +0000

    init.js

--- commands/admin/init.js
+++ commands/admin/init.js
@@ -1,33 +1,32 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const Discord = require("discord.js");
 
 module.exports = {
-    guildOnly: false, // サーバー専用コマンドかどうか
-    adminguildOnly: true,
-    data: new SlashCommandBuilder()
-        .setName("init")
-        .setDescription("サーバーでボットを使えるようにします。"),
+  guildOnly: false, // サーバー専用コマンドかどうか
+  adminguildOnly: true,
+  data: new SlashCommandBuilder()
+    .setName("init")
+    .setDescription("サーバーでボットを使えるようにします。"),
 
-    async execute(i, client) {
-        let category = i.guild.channels.cache.find(c => c.type == "GUILD_CATEGORY" && c.name == 'Bot Config');
-        let channels = i.guild.channels.cache.find(c => c.type == "GUILD_TEXT" && c.name == client.user.username);
-        if (category) {
-          console.log('1');
-            let rep = new Discord.EmbedBuilder().setTitle('このサーバーにはもう設定用のチャンネルがあります！！');
-            i.return({ embeds: [category] });
-            return 'No data';
-        } else {
-            i.guild.channels.create( { name:'Bot Config',type: 'GUILD_CATEGORY' });
-            //i.guild.channels.create(client.user.username);
-      i.guild.channels.create({ name: client.user.username, reason: 'Bot Config(Do　not edit！)' })
-            //let role = i.guilds.roles.fetch();
-            /*configCh.permissionOverwrites.set([
-                {
-                    id: role, // またはそれらのオブジェクト
-                    deny: 535529191505, // 許可しない権限
-                }
-            ], 'Config channel.');
-            i.reply('sucsess.');*/
-        }
-    },
+  async execute(i, client) {
+    let category = i.guild.channels.cache.find((c) => c.name == 'Bot Config' && c.type == 4);
+
+    let channels = i.guild.channels.cache.find((c) => c.name === `${client.user.id}`);
+
+    if (channels && category) {
+      let rep = new EmbedBuilder().setTitle('このサーバーにはもう設定用のチャンネルがあります！！');
+      i.reply({ embeds: [rep] });
+      return 'No data';
+    } else {
+      i.guild.channels.create({ name: 'Bot Config', type: 4 }).then(cate =>
+        i.guild.channels.create({ name: client.user.id, reason: 'Bot Config(Do　not edit！)', type: 0, parent: cate }).then(ch => ch.permissionOverwrites.set([{
+          id: i.guild.roles.everyone,
+          //すべての人(everyone)の権限設定
+          deny: ['67584']
+          //チャンネルを見ることを禁止する
+        }], 'Bot Config.')));
+      i.reply('sucsess.');
+    }
+    return 'No data';
+  },
 }
