7ffb23bcc0af6369937f4a05446ad4abfe0b7d86
SigumaRoot <sigumaroot@outlook.jp>
2022-09-04 04:02:59 +0900

    app

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const { ShardingManager, Shard } = require('discord.js');
 const manager = new ShardingManager('/bot.js',{token:process.env.token});
 
 manager.on('shardCreate',shard=>console.log(`Launched shard ${shard.id}`));
+
+// APIサーバー (UptimeRobot用)
+const express = require("express");
+const app = express();
+
+// ルーティングの設定
+app.get("/", (req, res) => {
+  const data = {
+    "Status": "OK",
+  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log(`Opened API Server`);
+});
