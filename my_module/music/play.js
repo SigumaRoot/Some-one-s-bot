43d0dd8135d85c2605d96aec177f3c5ca244dfc5
SigumaRoot <sigumaroot@outlook.jp>
2022-09-05 18:15:54 +0000

    miss

--- my_module/music/play.js
+++ my_module/music/play.js
@@ -1,44 +1,43 @@
 const { SlashCommandSubcommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandSubcommandBuilder()
-        .setName('play')
-        .setDescription('play')
-        .addStringOption(option => option.setName('url')),
-    async execute(i, client) {
-        let voiceChannel = i.member.voice.channel;
-        if (!voiceChannel) return i.reply(`You Need to Join Voice Channel`);
+  data: new SlashCommandSubcommandBuilder()
+    .setName('play')
+    .setDescription('play')
+    .addStringOption(option => option.setName('url')),
+  async execute(i, client) {
+    let voiceChannel = i.member.voice.channel;
+    if (!voiceChannel) return i.reply(`You Need to Join Voice Channel`);
 
-        let search_Song = args.join(" ");
-        if (!search_Song) return i.reply(`Type Song name or Link`);
+    let search_Song = args.join(" ");
+    if (!search_Song) return i.reply(`Type Song name or Link`);
 
-        let queue = player.createQueue(i.guild.id, {
-            metadata: {
-                channel: i.channel,
-            },
-        });
+    let queue = player.createQueue(i.guild.id, {
+      metadata: {
+        channel: i.channel,
+      },
+    });
 
-        // verify vc connection
-        try {
-            if (!queue.connection) await queue.connect(voiceChannel);
-        } catch {
-            queue.destroy();
-            return await i.reply({
-                content: "Could not join your voice channel!",
-                ephemeral: true,
-            });
-        }
-
-        let song = await player
-            .search(search_Song, {
-                requestedBy: i.author,
-            })
-            .then((x) => x.tracks[0]);
-        if (!song) return i.reply(` I cant Find \`${search_Song}\` `);
-
-        queue.play(song);
-
-        i.reply({ content: `⏱️ | Loading track **${song.title}**!` });
+    // verify vc connection
+    try {
+      if (!queue.connection) await queue.connect(voiceChannel);
+    } catch {
+      queue.destroy();
+      return await i.reply({
+        content: "Could not join your voice channel!",
+        ephemeral: true,
+      });
     }
-    }
+
+    let song = await player
+      .search(search_Song, {
+        requestedBy: i.author,
+      })
+      .then((x) => x.tracks[0]);
+    if (!song) return i.reply(` I cant Find \`${search_Song}\` `);
+
+    queue.play(song);
+
+    i.reply({ content: `⏱️ | Loading track **${song.title}**!` });
+  }
 }
