242463d36216b703e21cb570f55611fefa3469a8
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 10:44:26 +0900

    test

--- my_module/music/loop.js
+++ my_module/music/loop.js
@@ -1,12 +1,22 @@
 const { SlashCommandSubcommandBuilder } = require('discord.js');
+const { QueueRepeatMode } = require('discord-player');
 
 module.exports = {
     data: new SlashCommandSubcommandBuilder()
         .setName('loop')
-        .setDescription('ループ設定'),
+        .setDescription('ループ設定')
+        .addStringOption(option => 
+            option.setName('type')
+            .setDescription('タイプを選択')
+            .addChoice('TRACK')),
     async execute(i, client) {
+        let type;
+        switch(i.options.getString('type')){
+            case TRACK:
+                type = QueueRepeatMode.TRACK;
+        }
         let queue = client.player.getQueue(i.guild.id);
-        queue.setRepeatMode('TRACK');
+        queue.setRepeatMode(type);
         i.reply(`ループ設定完了！！次も同じ曲を再生します！！`);
     }
 }
