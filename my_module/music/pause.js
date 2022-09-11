febd32c0a85d159aa0239d31801cd518d05abe4e
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 15:17:43 +0900

    miss

--- my_module/music/pause.js
+++ my_module/music/pause.js
@@ -5,7 +5,7 @@ module.exports = {
         .setName('pause')
         .setDescription('一時停止'),
     async execute(i, client) {
-        let queue = client.player.getQueue(i.guild.id);        
+        let queue = client.player.getQueue(i.guild.id);
         if(!queue)return i.editReply('何も再生してません！！');
         queue.setPaused(true);
         i.editReply('一時停止中。。。再開するには`/music resume`を実行。');
