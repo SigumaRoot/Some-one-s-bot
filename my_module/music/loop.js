070933d8b139c7dc4c6a995dc0a4402d303b2686
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 11:04:23 +0900

    ok

--- my_module/music/loop.js
+++ my_module/music/loop.js
@@ -9,14 +9,33 @@ module.exports = {
             option.setName('type')
             .setDescription('タイプを選択')
             .addChoices(
-                { name: 'OFF', value: QueueRepeatMode.OFF },
-				{ name: 'TRACK', value: QueueRepeatMode.TRACK },
-				{ name: 'QUEUE', value: QueueRepeatMode.QUEUE },
-				{ name: 'AUTO PLAY', value: QueueRepeatMode.AUTOPLAY },
+                { name: 'OFF', value: 'OFF' },
+				{ name: 'TRACK', value: 'TRACK' },
+				{ name: 'QUEUE', value: 'QUEUE' },
+				{ name: 'AUTO PLAY', value: 'AUTOPLAY' },
 			)),
     async execute(i, client) {        
+        let type;
+        switch (i.options.getString(type)) {
+            case 'OFF':
+                type = QueueRepeatMode.OFF;
+                break;
+            case 'TRACK':
+                type = QueueRepeatMode.TRACK;
+                break;
+            case 'QUEUE':
+                type = QueueRepeatMode.QUEUE;
+                break;
+            case 'AUTOPLAY':
+                type = QueueRepeatMode.AUTOPLAY;
+                break;
+            default:
+                break;
+        }
+
+
         let queue = client.player.getQueue(i.guild.id);
-        queue.setRepeatMode(i.options.getString(type));
+        queue.setRepeatMode(type);
         i.reply(`ループ設定完了！！`);
     }
 }
