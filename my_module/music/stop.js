de92130a96ac5cca441cbe3aa8537e652441e8ce
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 10:20:29 +0900

    miss

--- my_module/music/stop.js
+++ my_module/music/stop.js
@@ -5,7 +5,7 @@ module.exports = {
         .setName('stop')
         .setDescription('ストップ（消す）'),
     async execute(i, client) {
-        let queue = client.client.player.getQueue(i.guild.id);
+        let queue = client.player.getQueue(i.guild.id);
         queue.stop();
         i.reply(`VCから抜けました！！`);
     }
