87ddc2ecb9c0b924db51ab7021c9f1bb363ed6b8
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 09:57:13 +0900

    miss

--- my_module/music/volume.js
+++ my_module/music/volume.js
@@ -4,11 +4,11 @@ module.exports = {
     data: new SlashCommandSubcommandBuilder()
         .setName('volume')
         .setDescription('volume')
-        .addStringOption(option => option.setName(Volume).setDescription(ボリュームを指定).setRequired(true)),
+        .addStringOption(option => option.setName(volume).setDescription(ボリュームを指定).setRequired(true)),
     async execute(i, client) {
         let queue = client.player.getQueue(i.guild.id);
-        let amount = parseInt(args[0]);
-        queue.setVolume(amount);
-        i.reply(`Volume Set to \`${amount}\``);
+        const vol = i.options.getString('volume');
+        queue.setVolume(vol);
+        i.reply(`ボリュームを \`${vol}\`に変更！！`);
     }
 }
