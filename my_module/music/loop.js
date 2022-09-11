861b679de86028427baaf8b346815b40ebbd993f
SigumaRoot <sigumaroot@outlook.jp>
2022-09-11 10:35:19 +0900

    test

--- my_module/music/loop.js
+++ my_module/music/loop.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('ループ設定'),
     async execute(i, client) {
         let queue = client.player.getQueue(i.guild.id);
-        queue.setRepeatMode(true);
+        queue.setRepeatMode('TRACK');
         i.reply(`ループ設定完了！！次も同じ曲を再生します！！`);
     }
 }
